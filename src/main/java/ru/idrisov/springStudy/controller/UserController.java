137a55d7dcfbee024a7c49d684729fc251667a9b
Ruslan Idrisov <idrisovruslan074@gmail.com>
2020-04-12 12:22:50 +0300

    V1.3

---
+++ src/main/java/ru/idrisov/springStudy/controller/UserController.java
@@ -0,0 +1,67 @@
+package ru.idrisov.springStudy.controller;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ru.idrisov.springStudy.domain.Role;
+import ru.idrisov.springStudy.domain.User;
+import ru.idrisov.springStudy.repos.UserRepo;
+
+
+@Controller
+@RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
+public class UserController {
+	@Autowired
+	private UserRepo userRepo;
+	
+	@GetMapping
+	public String userList(Model model) {
+		model.addAttribute("users", userRepo.findAll());
+		return "userList";
+	}
+	
+	@GetMapping("{user}")
+	public String userEditForm(Model model, @PathVariable User user) {
+		model.addAttribute("user", user);
+		model.addAttribute("roles", Role.values());
+		return "userEdit";
+	}
+	
+	@PostMapping
+	public String userSave(
+			@RequestParam String username,
+			@RequestParam Map<String, String> form,
+			@RequestParam("userId") User user
+			) {
+		
+		user.setUsername(username);
+		
+		Set<String> roles = Arrays.stream(Role.values())
+				.map(Role::name)
+				.collect(Collectors.toSet());
+		
+		user.getRoles().clear();
+		
+		for (String key : form.keySet()) {
+			if(roles.contains(key)) {
+				user.getRoles().add(Role.valueOf(key));
+			}
+		}
+		
+		userRepo.save(user);
+		return "redirect:/user";
+	}
+}
